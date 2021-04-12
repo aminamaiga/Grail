ec21f1159df06d1b278a477755221fb2405a03b0
aminata.maiga <veromaiga@gmail.com>
2021-04-12 20:28:29 +0200

    cap

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "RUN the 3 containers"
-docker run -d -it --name container1 aminavero/pyth:first || docker start container1 
+docker run -d -it --name container1 aminavero/pyth:latest || docker start container1 
 docker run -d -it --volumes-from container1 --name container2 efrecon/mini-tcl || docker start container2
 docker run -d -it --volumes-from container1 --name container3 swipl || docker start container3
 
-docker cp raw.txt container1:/app/GrailLight/raw.txt
+docker cp raw.txt container1:/app/raw.txt
  
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
@@ -13,7 +13,7 @@ else
     printf 'Error Running containers\n'
 fi 
 
-docker exec -it container2 sh -c '/app/GrailLight/tokenize.tcl  /app/GrailLight/raw.txt > /app/GrailLight/input.txt' 
+docker exec -it container2 sh -c '/app/tokenize.tcl  /app/raw.txt > /app/input.txt' 
 RESULT2=$?
 if [ $RESULT2 -eq 0 ]; then
     printf 'Tokenisation succeeded\n'
@@ -21,7 +21,7 @@ else
     printf 'Error Tokenisation\n'
 fi 
 
-docker exec -it container1 bash -c 'python3 elmo_superpos/super.py --input=../GrailLight/input.txt --output=/app/GrailLight/supertag.txt --beta=0.01'    
+docker exec -it container1 bash -c 'python3 elmo_superpos/super.py --input=../input.txt --output=/app/supertag.txt --beta=0.01'    
 RESULT3=$?
 if [ $RESULT3 -eq 0 ]; then
     printf 'Tagging succeeded\n'
@@ -29,7 +29,7 @@ else
     printf 'Error Tagging\n'
 fi 
 
-docker exec -it container2 sh -c 'tclsh /app/GrailLight/supertag2pl /app/GrailLight/supertag.txt > /app/GrailLight/superpos_nolem.pl'
+docker exec -it container2 sh -c 'tclsh /app/supertag2pl /app/supertag.txt > /app/superpos_nolem.pl'
 RESULT4=$?
 if [ $RESULT4 -eq 0 ]; then
     printf 'conversion in prolog succeeded\n'
@@ -39,22 +39,21 @@ fi
 
 docker exec -it container3 bash
 
+#swipl -s /app/lefff.pl -g "lemmatize." -t halt. 
+
 # commande à saisr namuellement
-#docker exec -it container3 bash -c ' swipl -s /app/GrailLight/lefff.pl -g "lemmatize(['./app/GrailLight/superpos_nolem.pl'])." -t halt. ' 
-# swipl -s /app/GrailLight/grail_light_nd.pl -g "compile('./app/GrailLight/superpos')." -t halt.
-# swipl -s /app/GrailLight/grail_light_nd.pl -g "chart_parse_all." -t halt.
-# exit
 
-#docker cp container1:/app/GrailLight/semantics.tex .
-#docker cp container1:/app/GrailLight/semantics.tex .
 
-#docker exec -it container1 bash -c 'pdflatex /app/GrailLight/semantics.tex' 
-#docker cp container1:/app/GrailLight/semantics.pdf .
+#docker cp container1:/app/semantics.tex .
+#docker cp container1:/app/semantics.tex .
+
+#docker exec -it container1 bash -c 'pdflatex /app/semantics.tex' 
+#docker cp container1:/app/semantics.pdf .
 #exit
-#docker exec -it container1 bash -c 'pdflatex /app/GrailLight/proof.tex'
+#docker exec -it container1 bash -c 'pdflatex /app/proof.tex'
 #exit
-#docker cp container1:/app/GrailLight/semantics.pdf .
-#docker cp container1:/app/GrailLight/proof.pdf .
+#docker cp container1:/app/semantics.pdf .
+#docker cp container1:/app/proof.pdf .
 
 
 
